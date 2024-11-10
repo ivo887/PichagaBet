ac8ca479cce6468cd7ce352bbfc68ca5b5915519
Martbul <martbul01@gmail.com>
2024-11-10 13:28:43 +0200

    successfuly compiling mainMenu and kazino

--- src/roulete/kazino.java
+++ src/roulete/kazino.java
@@ -237,26 +237,26 @@ public class kazino extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - ivaylo
+        // Generated using JFormDesigner Evaluation license - martin kov
         panel1 = new JPanel();
         textField1 = new JTextField();
         button1 = new JButton();
         moneyLabel = new JLabel();
         textField2 = new JTextField();
-        comboBox1 = new JComboBox(bet1);
+        comboBox1 = new JComboBox();
         label1 = new JLabel();
         label2 = new JLabel();
-        comboBox2 = new JComboBox(bet3);
+        comboBox2 = new JComboBox();
         panel2 = new JPanel();
 
         //======== panel1 ========
         {
             panel1.setBackground(Color.black);
-            panel1.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
-            0,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
-            .BOTTOM,new java.awt.Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt.Color.
-            red),panel1. getBorder()));panel1. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
-            beans.PropertyChangeEvent e){if("\u0062ord\u0065r".equals(e.getPropertyName()))throw new RuntimeException();}});
+            panel1.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder (
+            0, 0 ,0 , 0) ,  "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder
+            . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,java . awt. Color .
+            red ) ,panel1. getBorder () ) ); panel1. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java .
+            beans. PropertyChangeEvent e) { if( "\u0062ord\u0065r" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
 
             //---- textField1 ----
             textField1.setBackground(new Color(0x333333));
@@ -362,7 +362,7 @@ public class kazino extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - ivaylo
+    // Generated using JFormDesigner Evaluation license - martin kov
     private JPanel panel1;
     private JTextField textField1;
     private JButton button1;
